4e9a912fa12ac2ab7df565df0c9b2228adb96c3a
Jekt <jekt.tl@gmail.com>
2015-01-15 14:39:32 +0100

    DM Experiment - Build a dumb NodeJS server

---
+++ node-server/app.js
@@ -0,0 +1,5 @@
+var http = require('http');
+var server = http.createServer(function(req, res){
+	console.log('Server is listening.');
+	res.end('Request ' + req.method + ' ' + req.url + ' received at ' + Date());
+}).listen(1234, '127.0.0.1');
