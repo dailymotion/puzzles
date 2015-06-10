c404b7627fccd1a22028b43f42ecea423d9cdd36
romainberger <romain@romainberger.com>
2015-06-10 17:14:58 +0200

    Moving cat. idk

--- self-printing-cat/README.md
+++ self-printing-cat/README.md
@@ -6,6 +6,9 @@ Because ASCII cats are cool. Real cats too.
 
     $ node cat.js
 
+    # cat moving his tail here if you're into that kind of things
+    $ node moving-cat.js
+
 ## Browser
 
 Open `index.html` in a browser.
---
+++ self-printing-cat/moving-cat.js
@@ -0,0 +1,11 @@
+(function cat() {(function a(){b = (+new Date % 2 === 0) ? ' ' : '  ';console.log(('\u001B[2J' + cat).replace('(' + a + ')()', '').replace(/\/{2}/g, '  ').replace(/ /g, b));setTimeout(a, 100)})()
+   //  |\___/|
+   // =) ^Y^ (=
+   //  \  ^  /
+   //   )=*=(
+   //  /     \
+   //  |     |   _
+   //  | | | |  / )
+   //  | | |_| / /
+   //  /_/_/____/
+})()
